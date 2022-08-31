134bfc9e51b9e5526ed591c7fe2d8b3400fb2721
Yusuf SARITEPECİ <yusuf.saritepeci@tbtk.gov.tr>
2022-08-31 16:42:32 +0300

    Initial commit

---
+++ src/test/java/yte/thebackend/ThebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package yte.thebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThebackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
